b0c6f94f5340a6602637fe4e644960ccae0622dd
Brandon White <46477427+iambrandonwhite@users.noreply.github.com>
2019-05-04 23:24:54 -0700

    Add files via upload

---
+++ db/schema.sql
@@ -0,0 +1,12 @@
+-- Create a database called 'burger' and switch into it for this activity --
+DROP DATABASE IF EXISTS burgers_db;
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+-- Create a table called 'products' which will contain the store inventory --
+CREATE TABLE burgers (
+	id int NOT NULL AUTO_INCREMENT,
+	name varchar(255) NOT NULL,
+	devoured BOOLEAN DEFAULT false,
+	PRIMARY KEY (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Western Burger', false);
+INSERT INTO burgers (burger_name) VALUES ('Jalapeno Burger', false);
+INSERT INTO burgers (burger_name) VALUES ('Bacon Burger', false);
